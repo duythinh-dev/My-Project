cdb049e56156b6809867207ac2a611a8b84f2f2c
= <=>
2020-10-28 15:00:14 +0700

    rut gon code, map

--- my-app/src/components/Cards.js
+++ my-app/src/components/Cards.js
@@ -1,51 +1,95 @@
-import React from 'react'
-import CardItem from './CardItem'
-import './Cards.css'
-import img from '../images/img-9.jpg'
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
+import img from "../images/img-9.jpg";
+import img2 from "../images/img-6.jpg";
 function Cards() {
-    return (
-        <div className="cards">
-            <h1>Check out these EPIC Destinations!</h1>
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className='cards__items'>
-                        <CardItem 
-                        src={img}
-                        text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                        label="Adventure"
-                        path="/services"
-                        />
-                        <CardItem 
-                        src={img}
-                        text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                        label="Adventure"
-                        path="/services"
-                        />
-                    </ul>                   
-                    <ul className='cards__items'>
-                        <CardItem 
-                        src={img}
-                        text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                        label="Adventure"
-                        path="/services"
-                        />
-                        <CardItem 
-                        src={img}
-                        text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                        label="Adventure"
-                        path="/services"
-                        />
-                            <CardItem 
-                            src={img}
-                            text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                            label="Adventure"
-                            path="/services"
-                            />
-                    </ul>                   
-                </div>                
-            </div>            
+
+  const fakedata = [
+    {
+      src: img,
+      label: "Adventure",
+      path: "/services",
+      text: "Explore the hidden waterfall deep inside the Amazon Jungle",
+    },
+    {
+      src: img2,
+      label: "Adventure",
+      path: "/services",
+      text: "Explore the hidden waterfall deep inside the Amazon Jungle",
+    },
+  ];
+  const fakedata2 = [
+    {
+      src: img,
+      label: "Adventure",
+      path: "/services",
+      text: "Explore the hidden waterfall deep inside the Amazon Jungle",
+    },
+    {
+      src: img2,
+      label: "Adventure",
+      path: "/services",
+      text: "Explore the hidden waterfall deep inside the Amazon Jungle",
+    },
+    {
+      src: img,
+      label: "Adventure",
+      path: "/services",
+      text: "Explore the hidden waterfall deep inside the Amazon Jungle",
+    },
+  ];
+
+  return (
+    <div className="cards">
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            {fakedata.map((item, sa) => (
+              <CardItem key={(() => `${sa}`)()} {...item} />
+            ))}
+           
+            {/* <CardItem
+              src={img}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={img} 
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            /> */}
+          </ul>
+          <ul className="cards__items">
+            {fakedata2.map((item, xb) => (
+              <CardItem key={(() => `${xb}`)()} {...item}/>
+            ))}
+            {/* <CardItem
+              src={img}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={img}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={img}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            /> */}
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -5,12 +5,88 @@ import {Button} from './Button';
 import './Button.css'
 
 function Footer() {
+
+    const socialIcon = [
+        {
+            className:'social-icons-links facebook',
+            to:'/' ,
+            target:'_blank' ,
+            ariaLabel:'Facebook',
+            itemClass: "fab fa-facebook-f",
+        },
+        {
+            className:'social-icons-links instagram',
+            to:'/',
+            target:'_blank',
+            ariaLabel:'Instagram',
+            itemClass:'fab fa-instagram'
+        },
+        {
+            className:'social-icons-links youtube',
+            to:'/' ,
+            target:'_blank', 
+            ariaLabel:'Youtube',
+            itemClass:"fab fa-youtube",
+        },
+        {
+            className:'social-icons-links twitter',
+            to:'/' ,
+            target:'_blank', 
+            ariaLabel:'Twitter',
+            itemClass:"fab fa-twitter",
+        },
+        {
+            className:'social-icons-links linkedin',
+            to:'/' ,
+            target:'_blank', 
+            ariaLabel:'Linkedin',
+            itemClass:"fab fa-linkedin",
+        },
+    ]
+    const footerItem = [
+        {
+            className:"footer-link-items",
+            h2Text:"Contact Us",
+            linkText1:"Contact",
+            to1:"/",
+            linkText2:"Support",
+            to2:"/",
+            linkText3:"Destinations",
+            to3:"/",
+            linkText4:"Sponsorships",
+            to4:"/",
+        },
+        {
+            className:"footer-link-items",
+            h2Text:"Videos",
+            linkText1:"Submit Video",
+            to1:"/",
+            linkText2:"Ambassadors",
+            to2:"/",
+            linkText3:"Agency",
+            to3:"/",
+            linkText4:"Influencer",
+            to4:"/",
+        },
+        {
+            className:"footer-link-items",
+            h2Text:"Social Media",
+            linkText1:"Instagram",
+            to1:"/",
+            linkText2:"Facebook",
+            to2:"/",
+            linkText3:"Twitter",
+            to3:"/",
+            linkText4:"YouTube",
+            to4:"/",
+        },
+    ]
     return (
         <div className="footer-container">
-            <section className='footer-subscription'>
+            <section className='footer-subscription'>                
                 <p className='footer-subscription-heading'>
-                    Join the Adventure news letter to receive our best vacation deals.    
-                </p>
+                Join the Adventure news letter to receive our best vacation deals.
+                </p>               
                 <p className='footer-subscription-text'>
                     You can unsubscribe at any time
                 </p>
@@ -31,27 +107,15 @@ function Footer() {
                         <Link to='/'>Investors</Link>
                         <Link to='/'>Terms of Service</Link>
                     </div>
-                    <div className='footer-link-items'>
-                        <h2>Contact Us</h2>
-                        <Link to='/'>Contact</Link>
-                        <Link to='/'>Support</Link>
-                        <Link to='/'>Destinations</Link>
-                        <Link to='/'>Sponsorships</Link>
+                    {footerItem.map((item, sa) => (
+                    <div key={(() => `${sa}`)()} className={item.className}>
+                        <h2>{item.h2Text}</h2>
+                        <Link to={item.to1}>{item.linkText1}</Link>
+                        <Link to={item.to2}>{item.linkText2}</Link>
+                        <Link to={item.to3}>{item.linkText3}</Link>
+                        <Link to={item.to4}>{item.linkText4}</Link>
                     </div>
-                    <div className='footer-link-items'>
-                        <h2>Videos</h2>
-                        <Link to='/'>Submit Video</Link>
-                        <Link to='/'>Ambassadors</Link>
-                        <Link to='/'>Agency</Link>
-                        <Link to='/'>Influencer</Link>
-                    </div>
-                    <div className='footer-link-items'>
-                        <h2>Social Media</h2>
-                        <Link to='/'>Instagram</Link>
-                        <Link to='/'>Facebook</Link>
-                        <Link to='/'>Twitter</Link>
-                        <Link to='/'>YouTube</Link>
-                    </div>
+                    ))}                    
                 </div>                
             </div>
             <section className='social-media'>
@@ -63,36 +127,15 @@ function Footer() {
                     </div>
                     <small className='website-rights'>TRVL x 2020</small>
                     <div className='social-icons'>
-                        <Link className='social-icons-links facebook'
-                         to='/' 
-                         target='_blank' 
-                         aria-label='Facebook'>
-                            <i className='fab fa-facebook-f'></i>
-                        </Link>
-                        <Link className='social-icons-links instagram'
-                         to='/' 
-                         target='_blank' 
-                         aria-label='Instagram'>
-                            <i className='fab fa-instagram'></i>
-                        </Link>
-                        <Link className='social-icons-links youtube'
-                         to='/' 
-                         target='_blank' 
-                         aria-label='Youtube'>
-                            <i className='fab fa-youtube'></i>
-                        </Link>
-                        <Link className='social-icons-links twitter'
-                         to='/' 
-                         target='_blank' 
-                         aria-label='Twitter'>
-                            <i className='fab fa-twitter'></i>
-                        </Link>
-                        <Link className='social-icons-links linkedin'
-                         to='/' 
-                         target='_blank' 
-                         aria-label='Linkedin'>
-                            <i className='fab fa-linkedin'></i>
-                        </Link>
+                        {socialIcon.map((item, ix) => (
+                            <Link key={(() => `${ix}`)()} 
+                            className={item.className}
+                            aria-label={item.ariaLabel}
+                            target={item.target}
+                            to={item.to}>
+                                <i className={item.itemClass}></i>
+                            </Link>      
+                        ))}                        
                     </div>                    
                 </div>                
             </section>
